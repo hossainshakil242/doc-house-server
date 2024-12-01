97c935c37fb3abdafc9271d8ccf7789cbaab4e16
hossainshakil242 <hossainshakil242@gmail.com>
2024-12-01 18:07:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.vwvxcgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const tabCollection = client.db("DocHouse").collection("TabData");
+    const servicesCollection = client.db("DocHouse").collection("Services");
+
+    app.get('/tabData', async (req, res) => {
+      const result = await tabCollection.find().toArray();
+      res.send(result);
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send("doc-house is okey")
+})
+
+app.listen(port, () => {
+  console.log(`doc-house running on: ${port}`)
+})
+
